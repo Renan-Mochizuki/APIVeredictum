1995a87a183b0dc372ac2246706429a097620961
Renan-Mochizuki <renanmochizuki@gmail.com>
2025-11-07 22:03:28 -0300

    Fix responseArea e crud basico generalizado

--- controllers/categoriaController.js
+++ controllers/categoriaController.js
@@ -1,38 +1,22 @@
 const pool = require('../config/db');
+const { basicCrudController } = require('./factory');
 
 const itemName = 'categoria';
 const itemNamePlural = 'categorias';
 
-async function getCategorias(req, res) {
-  try {
-    const result = await pool.query('SELECT * FROM Categoria');
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Erro ao buscar ' + itemNamePlural + ':', error);
-    res.status(500).json({ error: 'Erro ao buscar ' + itemNamePlural });
-  }
-}
+const validationRules = {
+  cateNome: { required: true, type: 'string', minLength: 3, maxLength: 100 },
+};
 
-async function createCategoria(req, res) {
-  let { nome } = req.body;
+const { getAll, createItem } = basicCrudController({
+  table: 'Categoria',
+  idCol: 'cateId',
+  itemName,
+  itemNamePlural,
+  fieldsCreate: ['cateNome'],
+  fieldsUpdate: ['cateNome'],
+  validationRulesCreate: validationRules,
+  validationRulesUpdate: validationRules,
+});
 
-  const validationRules = {
-    nome: { required: true, type: 'string', minLength: 3, maxLength: 30 },
-  };
-
-  try {
-    const { isValid, errors } = validateData(req.body, validationRules);
-
-    if (!isValid) {
-      return res.status(400).json({ error: `Dados inválidos: ${errors.join(', ')}` });
-    }
-    
-    const result = await pool.query('INSERT INTO Categoria (catNome) VALUES ($1) RETURNING *', [nome]);
-    res.status(201).json(result.rows[0]);
-  } catch (error) {
-    console.error('Erro ao criar ' + itemName + ':', error);
-    res.status(500).json({ error: 'Erro ao criar ' + itemName });
-  }
-}
-
-module.exports = { getCategorias };
+module.exports = { getAll, createItem };
